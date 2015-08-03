84a3288bd1136a4bcb2e2d181f715588f13a965d
ettoolong <ettoolong@hotmail.com>
2015-08-03 14:19:31 +0800

    Add Status, onSuccess and onError events.

--- src/firefoxos/SocketProxy.js
+++ src/firefoxos/SocketProxy.js
@@ -1,4 +1,3 @@
-//TODO: we need add below setting to platforms/firefoxos/www/manifest.webapp
 //    "type": "privileged",
 //    "permissions": {
 //      "tcp-socket": {
@@ -9,21 +8,34 @@
 //    <platform name="firefoxos">
 //        <permission name="tcp-socket" description="Create TCP sockets and communicate over them." privileged="true"/>
 //    </platform> //
+//var socket_status = Socket().State;
 
 function TcpSocket() {
   this.onData = null;
   this.onError = null;
   this.onClose = null;
   this.socket = null;
+  this.writable = false;
+  this.state = TcpSocket.State.CLOSED;
 }
 
+TcpSocket.State = {};
+TcpSocket.State[Socket.State.CLOSED = 0] = "CLOSED";
+TcpSocket.State[Socket.State.OPENING = 1] = "OPENING";
+TcpSocket.State[Socket.State.OPENED = 2] = "OPENED";
+TcpSocket.State[Socket.State.CLOSING = 3] = "CLOSING";
+
 TcpSocket.prototype={
+  //Ref: https://developer.mozilla.org/en-US/docs/Web/API/TCPSocket
   open: function(host, port, onStartRequest, onSocketError){
     this.onStartRequest = onStartRequest;
     this.onSocketError = onSocketError;
+    this.state = TcpSocket.State.OPENING;
     this.socket = navigator.mozTCPSocket.open(host, port, {binaryType: 'arraybuffer'});
 
     this.socket.onopen = function(){
+      this.writable = true;
+      this.state = TcpSocket.State.OPENED;
       if(this.onStartRequest)
         this.onStartRequest();
     }.bind(this);
@@ -40,28 +52,42 @@ TcpSocket.prototype={
     }.bind(this);
 
     this.socket.onclose = function(){
+      this.state = TcpSocket.State.CLOSED;
       if(this.onClose) {
         this.onClose();
       }
     }.bind(this);
   },
 
-  write: function(data){
-    if(this.socket)
-      this.socket.send(data.buffer);
-  },
-
-  shutdownWrite: function(){
-    if(this.socket) {
-      this.socket.close();
-      this.socket = null;
+  write: function(data, onSuccess, onError){
+    if(this.socket && this.writable) {
+      try{
+        this.socket.send(data.buffer);
+        onSuccess();
+      } catch (ex) {
+        onError(ex.toString());
+      }
     }
   },
 
-  close: function(){
+  shutdownWrite: function(onSuccess, onError){
+    this.writable = false;
+  },
+
+  close: function(onSuccess, onError){
+    this.state = TcpSocket.State.CLOSING;
     if(this.socket) {
-      this.socket.close();
-      this.socket = null;
+      try{
+        this.socket.close();
+        this.socket = null;
+        if(onSuccess) {
+          onSuccess();
+        }
+      } catch (ex) {
+        if(onError) {
+          onError(ex.toString());
+        }
+      }
     }
   }
 };
